cd977800c9a79b2c493894129788b576a9ba2db7
Abdulrahim <abdulrahimsakr01@gmail.com>
2025-12-20 22:35:26 +0200

    fix bugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,5 @@
-import Image from "next/image";
-import { Button } from "@/src/components/ui/button";
-import Login from "./(pages)/login/page";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  const x = 10;
-  return (
-    <>
-      <div>hello</div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <div>
-        <Button>Click me</Button>
-      </div>
-    </>
-  );
+  redirect("/home");
 }
